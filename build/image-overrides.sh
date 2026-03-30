6b3ee4683fdca8175cc74619b0affa34f27b1c6e
Rich Renomeron <rrenomeron@gmail.com>
2026-03-30 11:26:53 -0400

    feat: add gnome backgrounds

--- build/image-overrides.sh
+++ build/image-overrides.sh
@@ -20,3 +20,13 @@ systemctl disable docker.service
 
 echo "Enabling Flatpak Preinstall"
 systemctl enable flatpak-preinstall.service
+
+echo "Installing GNOME Wallpaper"
+curl https://gitlab.gnome.org/GNOME/gnome-backgrounds/-/archive/50.0/gnome-backgrounds-50.0.zip \
+    > /tmp/gnome-backgrounds-50.0.zip
+cd /tmp
+unzip gnome-backgrounds-50.0.zip
+
+mkdir /usr/share/backgrounds/gnome
+cd gnome-backgrounds-50.0/backgrounds
+cp *.jxl *.png *.svg /usr/share/backgrounds/gnome
